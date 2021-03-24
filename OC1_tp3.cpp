cb904064c0caeeae46642cbbbdf51f95c2e80ed3
samuel_assis <samuelassisvieira@gmail.com>
2021-03-24 16:08:37 -0300

    Finalizado e testado

--- OC1_tp3.cpp
+++ OC1_tp3.cpp
@@ -7,18 +7,17 @@
 
 using namespace std;
 
-struct instructions{
+struct Instruction{
 	int N;
 	int op;
 	string data = "0";
 	string str;
 };
 
-struct block{
+struct Block{
 	bool v;
 	bool dirty;
 	int tag;
-	int data;
 	int word0; //32 bit -> 4 words
 	int word1;
 	int word2;
@@ -28,17 +27,27 @@ struct block{
 
 struct Memory{
 	int mem_data[1024];   // 4096/4
-	block cache[64]; // 256/4
+	Block cache[64]; // 256/4
 	int hits;
 	int misses;
 	int reads;
 	int writes;
 	Memory();
-	void write(int,int,int,string);
-	bool read(int,int,int,int,int&);
+	void write(int,int,int,int);
+	bool read(int,int,int,int&);
+	void mem_search(int, int);
 	void write_back(int);
+	// void print_cache();
 };
-
+/*
+void Memory::print_cache(){
+	cout<<"CACHE: \n";
+	for(int i=0; i<64;i++){
+		cout<<"{"<<i<<"}"<<" (V: "<<cache[i].v<<" Dy: "<<cache[i].dirty<<" T: "<<cache[i].tag<<" ";
+		cout<<"-> "<<cache[i].word0<<" "<<cache[i].word1<<" "<<cache[i].word2<<" "<<cache[i].word3<<")"<<endl;
+	}
+}
+*/
 Memory::Memory(){
 	hits = misses = reads = writes = 0;
 	for(int i = 0; i<256;i++){
@@ -47,19 +56,19 @@ Memory::Memory(){
 		cache[i].v = false;
 		cache[i].dirty = false;
 		cache[i].tag = 0;
-		data = 0;
 		cache[i].word0 = cache[i].word1 = cache[i].word2 = cache[i].word3 = 0;
 		}
 	}
 }
 void Memory::write_back(int i){
-	int t = cache[idx].tag;
-		int addr = t*256 + idx*4;
-		mem_data[addr]   = cache[idx].word0;
-		mem_data[addr+1] = cache[idx].word1;
-		mem_data[addr+2] = cache[idx].word2;
-		mem_data[addr+3] = cache[idx].word3;
-		cache[idx].dirty = false;
+	int t = cache[i].tag;
+	int addr = t*256 + i*4;
+
+	mem_data[addr]   = cache[i].word0;
+	mem_data[addr+1] = cache[i].word1;
+	mem_data[addr+2] = cache[i].word2;
+	mem_data[addr+3] = cache[i].word3;
+	cache[i].dirty = false;
 }
 void Memory::mem_search(int t, int i){
 	int mem_address =  i*4 + t*256; // adress = index*2² + tag*2⁸
@@ -70,14 +79,14 @@ void Memory::mem_search(int t, int i){
 	cache[i].word2 = mem_data[mem_address+2];
 	cache[i].word3 = mem_data[mem_address+3];
 }
-void Memory::write(int tg, int idx, int off, string data){
+void Memory::write(int tg, int idx, int off, int data){
 	
 	this->writes++;
 
-	if(this->cache[idx].v == false || cache[idx] != tag){ //buscar valor na memoria;
-		mem_search(tg,int idx);
+	if(this->cache[idx].v == false || cache[idx].tag != tg){ //buscar valor na memoria;
+		mem_search(tg,idx);
 	}
-	if(this->cache[idx].dirty = true){	//atualizar valor na memoria antes de escrever;
+	if(this->cache[idx].dirty == true){	//atualizar valor na memoria antes de escrever;
 		write_back(idx);
 	}
 	// escrita;
@@ -90,78 +99,80 @@ bool Memory::read(int tg, int idx, int off, int& val){
 
 	if(cache[idx].v == true && cache[idx].tag == tg){
 		val = cache[idx].word0;
-		this->hits ++;
 		return true;
 	}
+
 	if(cache[idx].v == true && cache[idx].tag != tg){
 		if(cache[idx].dirty == false){ //busca na memoria
-		mem_search(tg,idx);
-		val = cache[idx].word0;
-		this->misses++;
-		return false;
+			mem_search(tg,idx);
+			val = cache[idx].word0;
+			return false;
 		}else{ //atualiza a memoria
 			write_back(idx);
 			mem_search(tg,idx);
 			val = cache[idx].word0;
-			this->misses++;
+
 			return false; 
 		}
 	}
+	return false;
 }
 
 int main(){
-	int adress, op_bit,tag, index,offset, value,d ,n_access = 0;
-	bool hit = false;
+	int address, op_bit,tag, index,offset, value,d ,n_access = 0;
 	Memory mem;
-	string writing_data, bin_adress;
-	list<instructions> input_data;
-	instructions inst;
-
-	while(scanf("%d %d", &adress,&op_bit) != EOF){
-		bin_adress = bitset<32>(adress).to_string();
-		// cout<<"bin_adress = "<<bin_adress<<endl;
-		tag = (int) (bitset<22>(bin_adress.substr(0,21)).to_ulong());    // 22 bits tag
-		index = (int) (bitset<6>(bin_adress.substr(22,27)).to_ulong());  // 6  bits indice
-		offset = (int) (bitset<4>(bin_adress.substr(28,31)).to_ulong()); // 4  bits offset
-		// cout<<"tag = "<<tag<<"\nidx = "<<index<<"\noffset = "<<offset<<endl;
+	string writing_data, bin_address;
+	list<Instruction> input_data;
+	Instruction inst;
+	bool hit;
+	while(scanf("%d %d", &address,&op_bit) != EOF){
+		bin_address = bitset<32>(address).to_string();
+		//cout<<"adress"<<"bin_adress = "<<bin_adress<<endl;
+		tag = (int) (bitset<22>(bin_address.substr(0,21)).to_ulong());    // 22 bits tag
+		index = (int) (bitset<6>(bin_address.substr(22,27)).to_ulong());  // 6  bits indice
+		offset = (int) (bitset<4>(bin_address.substr(28,31)).to_ulong()); // 4  bits offset
+		// cout<<"{"<<count<<"}\n"<<"tag = "<<tag<<"\nidx = "<<index<<"\noffset = "<<offset<<endl;
 		if(op_bit){
 			getline(cin,writing_data);
-			d = (int)(bitset<32>(writing_data).to_ulong());
+			d = stoi(writing_data,0,2);
 			mem.write(tag,index,offset,d);
 			inst.data = writing_data;
-			inst.srt = "W";
+			inst.str = "W";
 		}else{
-			n_acess++;
-			if(mem.read(tag,index,offset,value)){
+			n_access++;
+			hit = mem.read(tag,index,offset,value);
+			if(hit){
 				inst.str = "HIT";
+				mem.hits++;
 			}else{
 				inst.str = "MISS";
+				mem.misses++;
 			}
 		}
-	}
-
 		//salvar os dados de entrada
-		inst.N = adress;
+		inst.N = address;
 		inst.op = op_bit;
-		input_data.insert(inst);
+		input_data.push_back(inst);
+	}
 
 		// escrever no arquivo
 		ofstream out_file;
-		out_file.open("result.txt");
-		cout<<"READS: "<<mem.reads<<endl;
-		cout<<"WRITES: "<<mem.writes <<endl;
-		cout<<"HITS: "<<mem.hits<<endl;
-		cout<<"MISSES: "<<mem.misses<<endl;
-		cout<<"HIT RATE: "<<mem.hits/n_access <<endl;
-		cout<<"MISS RATE: "<<mem.misses/n_access<<endl;
+		out_file.open("results.txt");
+		out_file<<"READS: "<<mem.reads<<" "<<endl;
+		out_file<<"WRITES: "<<mem.writes <<endl;
+		out_file<<"HITS: "<<mem.hits<<endl;
+		out_file<<"MISSES: "<<mem.misses<<endl;
+		out_file<<"HIT RATE: "<<(float)mem.hits/n_access<<endl;
+		out_file<<"MISS RATE: "<<(float)mem.misses/n_access<<endl;
+		out_file<<"\n \n";
 
-		list::iterator it;
-		for(it = inst.begin();it != inst.end();it++){
-			cout<<it*.N<<" "<<it*.op<<" ";
-			if(it*.data != "0") cout<<it*.data<<" ";
-			cout<<it*.str<<endl;
+		list<Instruction>::iterator it;
+		for(it = input_data.begin();it != input_data.end();it++){
+			out_file<<(*it).N<<" "<<(*it).op<<" ";
+			if((*it).op){ out_file<<(*it).data<<" ";}
+			out_file<<(*it).str<<endl;
 		}
-
+		// mem.print_cache();
 		out_file.close();
 
 	return 0;
